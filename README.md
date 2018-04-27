18996d13e2df4787c0b8210ab9c0ee389a995cf7
Mehdi <roadmate.limited@gmail.com>
2018-04-27 11:58:00 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,83 @@
+# surround-me README
+
+This is the README for your extension "surround-me". 
+This extension can help you improve your productivity by providing basic operations :
+Suitable for Javascript and typescript.
+
+## Features
+
+Surround a text block with the following :
+
+if(expression){
+	//your
+	//selected code
+	//here
+}else{
+	
+}
+or 
+
+try{
+	//your
+	//selected code
+	//here
+}catch(e){
+	cosole.error(e);
+	
+}
+or
+JSON.parse(<yourSelectedExpressionHere>)
+or
+JSON.stringify(<yourSelectedExpressionHere>)
+
+More surrounding expression will be available in the next versions
+
+## Requirements
+
+You need to always select your code from start to end. so that your cursor is at the end of the code you want to surround.
+Undo (ctrl+Z or cmd+z) will remove the whole operation.
+
+## Extension Settings
+
+For example:
+
+To surround a block with if/else :
+
+1- surround your code 
+2- (Windows => ctrl+shit+i) or (mac => cmd+shift+i)
+
+To surround a block with try/catch :
+
+1- surround your code 
+2- (Windows => ctrl+shit+t) or (mac => cmd+shift+t)
+
+To surround a block with JSON.parse(code) :
+
+1- surround your code 
+2- (Windows => ctrl+shit+p) or (mac => cmd+shift+p)
+
+To surround a block with JSON.stringify(code) :
+
+1- surround your code 
+2- (Windows => ctrl+shit+j) or (mac => cmd+shift+j)
+
+
+
+
+## Known Issues
+
+Selecting a block from end to start will reject the operation and display a message.
+Also I did not test this extension on Mac or Linux. So sorry in advance if there are bugs there.
+
+## Release Notes
+
+Users appreciate release notes as you update your extension.
+
+### 0.0.1
+
+Initial release of surround-me
+
+
+
+-----------------------------------------------------------------------------------------------------------
+
---
+++ src/extension.ts
@@ -0,0 +1,118 @@
+'use strict';
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+    let isTextCorrectlySelected = () =>{
+        let selection = vscode.window.activeTextEditor.selection;
+        let line_start = selection.start.line;
+        let char_Start = selection.start.character;
+        let cursorLine = selection.active.line;
+        let cursorChar = selection.active.character
+        if(line_start == cursorLine && char_Start == cursorChar){
+            vscode.window.showInformationMessage('Please select text from start to end. not the other way around.');
+            return false;
+        }
+        return true;
+    }
+    
+    let surroundWithFunction = (f) =>{
+        let selection = vscode.window.activeTextEditor.selection;
+        let line_start = selection.start.line;        
+        let char_numb_start = selection.start.character;  
+        
+        if(!isTextCorrectlySelected()){            
+            return;
+        }
+        let left_snippet = new vscode.SnippetString(`${f}(`);
+        let right_snippet = new vscode.SnippetString(")");
+        let firstLine = new vscode.Position(
+            line_start,
+            char_numb_start);
+        let EndLine = new vscode.Position(
+            line_start,
+            selection.active.character);
+        vscode.window.activeTextEditor.insertSnippet(right_snippet,EndLine,{ undoStopBefore: false, undoStopAfter: false });
+        vscode.window.activeTextEditor.insertSnippet(left_snippet,firstLine,{ undoStopBefore: false, undoStopAfter: false });
+    }
+    let twoBlocksSnippet = (firstBlock,secondBlock) =>{
+        if(!isTextCorrectlySelected()){            
+            return;
+        }
+        let line_start = vscode.window.activeTextEditor.selection.start.line;
+        let char_numb_start = vscode.window.activeTextEditor.selection.start.character;
+            // let line_end = vscode.window.activeTextEditor.selection.end.line;
+            // let all_text = vscode.window.activeTextEditor.document.getText();
+            let selection = vscode.window.activeTextEditor.selection;            
+            let addLines = 0
+            let if_snippet = new vscode.SnippetString(`${firstBlock}\r\n`);
+            let firstLine = new vscode.Position(
+                selection.start.line,
+                selection.start.character);
+            vscode.window.activeTextEditor.insertSnippet(if_snippet,firstLine,{ undoStopBefore: false, undoStopAfter: false });
+            addLines++;
+            let tabSnippet = new vscode.SnippetString("\t");
+            for(let i = selection.start.line + 1;i<=selection.active.line +1 ;i++){
+                let newline = new vscode.Position(
+                    i,
+                    selection.start.character);
+                vscode.window.activeTextEditor.insertSnippet(tabSnippet,newline,{ undoStopBefore: false, undoStopAfter: false });
+            }
+
+            let close_if_snippet = new vscode.SnippetString(`${secondBlock}\r\n\t\r\n}`);
+            // let close_all_snippet = new vscode.SnippetString("}");            
+            let lastLine = new vscode.Position(
+                selection.active.line +2 ,
+                selection.start.character);
+            vscode.window.activeTextEditor.insertSnippet(close_if_snippet,lastLine,{ undoStopBefore: false, undoStopAfter: false });
+            let newCursorPosition = vscode.window.activeTextEditor.selection.active.with(line_start,char_numb_start + 13)
+            var newSelection = new vscode.Selection(newCursorPosition, newCursorPosition);
+            vscode.window.activeTextEditor.selection = newSelection;
+    }
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with  registerCommand
+    // The commandId parameter must match the command field in package.json
+    let ifMe = vscode.commands.registerCommand('extension.ifMe', () => {
+                
+        if(!vscode.window.activeTextEditor.selection.isEmpty){
+            twoBlocksSnippet("if(expression){","}else{")            
+        }else{
+            vscode.window.showInformationMessage('no text selected');
+        }        
+
+    });
+    let tryCatchMe = vscode.commands.registerCommand('extension.tryCatchMe', () => {
+        if(!vscode.window.activeTextEditor.selection.isEmpty){
+            twoBlocksSnippet("try{","}catch(e){\r\n\tconsole.error(e);")            
+        }else{
+            vscode.window.showInformationMessage('no text selected');
+        }
+    });
+    let stringifyMe = vscode.commands.registerCommand('extension.stringifyMe', () => {        
+        if(!vscode.window.activeTextEditor.selection.isEmpty){
+            surroundWithFunction("JSON.stringify");
+        }else{
+            vscode.window.showInformationMessage('no text selected');
+        }
+    }); 
+
+    let parseMe = vscode.commands.registerCommand('extension.parseMe', () => {        
+        if(!vscode.window.activeTextEditor.selection.isEmpty){
+            surroundWithFunction("JSON.stringify");
+        }else{
+            vscode.window.showInformationMessage('no text selected');
+        }
+    }); 
+    
+    context.subscriptions.push(ifMe);
+    context.subscriptions.push(tryCatchMe);
+    context.subscriptions.push(stringifyMe);
+    context.subscriptions.push(parseMe);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+}
